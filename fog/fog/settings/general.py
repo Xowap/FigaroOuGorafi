50bfe5801307403a3a9f7bbc613956fc6944d8c0
Rémy Sanchez <remy.sanchez@hyperthese.net>
2013-10-23 22:47:37 +0200

    Starting to have some kind of a HTML template

--- fog/fog/settings/general.py
+++ fog/fog/settings/general.py
@@ -108,6 +108,7 @@ INSTALLED_APPS = (
 
     'south',
 
+    'fog',
     'guesser',
 )
 
--- fog/guesser/urls.py
+++ fog/guesser/urls.py
@@ -1,11 +1,11 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import TemplateView
 
 admin.autodiscover()
 
 urlpatterns = patterns(
-    '',
+    'guesser',
 
-    url(r'^admin/', include(admin.site.urls)),
-    url(r'^', include('guesser.urls')),
+    url(r'^$', TemplateView.as_view(template_name='base.html')),
 )
